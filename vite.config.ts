426aa92c29542df8f365a9bb263f2df579278631
Sachin Kumawat <sachinkumawat5311@gmail.com>
2026-09-19 22:35:26 +0530

    init: setup initial project structure and package.json configuration

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import wasm from 'vite-plugin-wasm';
+import topLevelAwait from 'vite-plugin-top-level-await';
+
+export default defineConfig({
+  plugins: [react(), wasm(), topLevelAwait()],
+  server: {
+    port: 5173
+  },
+  build: {
+    rollupOptions: {
+      external: ['crypto', 'node:fs/promises', 'node:path']
+    }
+  }
+});
